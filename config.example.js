963dd96aa39fdeda9d32af6e312fc7bcac463e98
Screeny <screeny05@gmail.com>
2014-07-08 15:15:13 +0200

    removed credentials from config.example.js

--- config.example.js
+++ config.example.js
@@ -31,8 +31,8 @@ module.exports = {
     //  - a Private Token = accessToken
     // Thanks, Chute!
     avatars_io:{
-        appID: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcml2YXRlX3Rva2VuIjoiNjg4MGMwMjYwZTU0NzA4YTYxMGI2NzQ4ZWZiZmJhZDg1NGMwNWM1ZTFmZmIxNDFlNzRlYWIzYWQ1NjljMmNlOSJ9.dldkN0gVgBq4LuqWKdJo7mRAKo5uQZisGYt0mqM_V34",
-        accessToken: "6880c0260e54708a610b6748efbfbad854c05c5e1ffb141e74eab3ad569c2ce9"
+        appID: "",
+        accessToken: ""
     },
     // For further reference see:
     // http://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html#the-url-connection-format
